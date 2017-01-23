dee9a560b17d355462f401eb25e4d92c0368ab1c
preeti-daffodil <preeti.gulia@daffodilsw.com>
2017-01-23 17:36:42 +0530

    k

--- main.js
+++ main.js
@@ -37,22 +37,30 @@ app.all('/updateServer',(req,res,err)=>{
 	console.log(' inside update call'+req.param('parameters'));
 	var clientParams=JSON.parse(req.param('parameters'))
 	console.log('port '+clientParams.port)
-	 var args = "git pull origin master";
 	 var cwd=''
 	if(clientParams.port=='6010'){
-		cwd="/media/preeti/4ff2a78e-a905-4e4c-898a-14ed3713b6cb/data/programming/UpdateVariousServersAndSettingDefaultProxy/serverone/proxy-server-new";
+		cwd=__dirname+"/serverone/proxy-server-new/";
 	}else if(clientParams.port=='6020'){
-		cwd="/media/preeti/4ff2a78e-a905-4e4c-898a-14ed3713b6cb/data/programming/UpdateVariousServersAndSettingDefaultProxy/servertwo/proxy-server-new";
+		cwd=__dirname+"/servertwo/proxy-server-new/";
 	}
     var out = {};
-    var exec = ChildProcess.exec;
-    exec(args, {cwd: cwd}, function (error, stdout, stderr) {
-        out.error = error;
-        out.stdout = stdout;
-        out.stderr = stderr;
-        console.log("this is my output >> >> "+JSON.stringify(out));
-        res.send(out);
-    });
+    var execFile=ChildProcess.execFile;
+    execFile('sh',[cwd+'/script.sh'],(err,stdout,stderr)=>{
+    	process.stdout.write('output >> >> '+stdout);
+    	out['error']=err;
+    	out['stdout']=stdout;
+    	out['stderr']=stderr;
+    	res.send(out);
+    })
+	 // var args = "git status";
+    // var exec = ChildProcess.exec;
+    // exec(args, {cwd: cwd}, function (error, stdout, stderr) {
+    //     out.error = error;
+    //     out.stdout = stdout;
+    //     out.stderr = stderr;
+    //     console.log("this is my output >> >> "+JSON.stringify(out));
+    //     res.send(out);
+    // });
 })
 
 var serverData={
---
+++ test.js
@@ -0,0 +1,35 @@
+
+
+
+var childProcess=require('child_process');
+
+var execFile=childProcess.execFile;
+var filepath=__dirname+'/script.sh';
+console.log('filepath >> >> '+filepath);
+var execFileResult=execFile('sh',[filepath],(err,stdout,stderr)=>{
+	// console.log('final data '+stdout)
+})
+execFileResult.stdout.on('data',(data)=>{
+	console.log('final data '+data)
+})
+
+
+// var spawn=childProcess.spawn;
+// if(process.argv[2]==='child'){
+// 	process.stdout.write('i m inside child');
+// }else{
+// 	var output=spawn(process.execPath,['test.js','child'],{
+// 		stdio:'inherit' //ignore can also be used
+// 	});
+// 	// output.stdout.pipe(process.stdout);
+// 	// output.stdout.on("data",(data)=>{
+// 	// 	console.log('from child '+data);
+// 	// })
+// }
+
+
+// var exec=childProcess.exec;
+// // exec('cat test.js',(err,stdout,stderr)=>{
+// // 	process.stdout.write('m done with cat commnad'+stdout);
+// // });
+
